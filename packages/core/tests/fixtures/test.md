7100ab65cf1dd3c44765ad36ae1659fde837ddab
ben-sentenac <bsr-web-09@proton.me>
2025-03-18 15:32:04 +0100

    chore:add workspace.yaml

---
+++ packages/core/tests/fixtures/test.md
@@ -0,0 +1,6 @@
+---
+title: "Bienvenue sur Pandora"
+date: "2025-03-18"
+---
+# Bienvenue sur mon site
+Voici un site généré avec **Pandora**.
---
+++ packages/core/tests/readMarkdown.test.ts
@@ -0,0 +1,8 @@
+import  assert  from "node:assert"
+import test from "node:test";
+
+
+test("Read markdown file", async () => {
+    //
+    assert.ok(true);
+});
